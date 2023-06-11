f544a9d6ab94940a4181edaf0488fe117b489937
Umang singh Rajawat <umangchetna@gmail.com>
2023-06-11 18:58:53 +0530

    reduced the size of showProducts

--- src/components/common/Tabs.tsx
+++ src/components/common/Tabs.tsx
@@ -19,19 +19,6 @@ export default function TransparentTabs({ data }: any) {
       value: "overview",
       desc: <OverviewTable data={data} />,
     },
-    {
-      label: "Intends",
-      value: "intends",
-      desc: `Because it's about motivating the doers. Because I'm here
-      to follow my dreams and inspire other people to follow their dreams, too.`,
-    },
-    {
-      label: "Data",
-      value: "data",
-      desc: `We're not always in the position that we want to be at.
-      We're constantly growing. We're constantly making mistakes. We're
-      constantly trying to express ourselves and actualize our dreams.`,
-    },
   ];
 
   useEffect(() => {}, []);
