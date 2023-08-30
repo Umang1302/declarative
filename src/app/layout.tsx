66e54ca1b4fd6e72ed6c8ea5e5f73635a8fd3d68
Umang Rajawat <umang.rajawat@gupshup.io>
2023-08-30 15:40:08 +0530

    favicon for apple

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,18 @@
-"use client";
-
 import "./globals.css";
 
 import React, { useEffect } from "react";
 
 import { Cabin } from "next/font/google";
 const cabin = Cabin({ subsets: ["latin"] });
-import { usePathname } from "next/navigation";
-import Head from "next/head";
+
+export const metadata = {
+  icons: {
+    icon: './icon.svg',
+    shortcut: './icon.svg',
+    apple: './apple-icon.png',
+  },
+}
+
 
 export default function Landingpage({
   children,
