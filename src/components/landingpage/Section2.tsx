b7765443b5ae7ef09f85824ae1a9a08726ec0e5e
Umang Rajawat <umang.rajawat@gupshup.io>
2023-08-31 16:42:03 +0530

    repetation fixed of tiles

--- src/components/landingpage/Section2.tsx
+++ src/components/landingpage/Section2.tsx
@@ -14,36 +14,29 @@ Managed Micro Data Products`,
     content: `Convert your enterprise data into an intelligent network of totally managed, business aligned, metadata driven, collaborative and inter connected data products.`,
   },
   {
-    heading: `Industry's best Data Management frameworks applied to Micro 
-    Data Products`,
+    heading: `Industry's best Data Management frameworks and features applied with simple Intents`,
     content: (
       <p>
-        Dome combines the best of Data Management, Data Mesh and Data Fabric
-        principles in a single platform. It comes equipped with Industry&apos;s
-        best Data Management Frameworks & Controls that are applied to data
-        using simple declarative Intents.
+       Dome is equipped with a rich library of Industry&apos;s best Data Management Frameworks, Principles, and Controls. Apply these controls to your Data Products using simple declarative Management Intents.
       </p>
     ),
   },
   {
-    heading: `Intelligent Network of
-Managed Micro Data Products`,
-    content: `Convert your enterprise data into an intelligent network of totally managed, business aligned, metadata driven, collaborative and inter connected data products.`,
+    heading: `Complete Observability, Auditability
+    and Compliance`,
+    content: `Dome provides the most comprehensive Observability, Auditability, & Compliance for your enterprise data by applying Framework controls to each individual product using declarative Management Intents. Each Dome product is independently Observable and Auditable.`,
   },
   {
-    heading: `Intelligent Network of
-Managed Micro Data Products`,
-    content: `Convert your enterprise data into an intelligent network of totally managed, business aligned, metadata driven, collaborative and inter connected data products.`,
+    heading: `Data Economy via Dome's Marketplace`,
+    content: `Get full transparency into Supply and Demand of your Data Products in order to best manage your ever growing enterprise data. Business value metrics & Impact of data products are calculated using supply and demand indicators to auto categorize products in criticality tiers.`,
   },
   {
-    heading: `Intelligent Network of
-Managed Micro Data Products`,
-    content: `Convert your enterprise data into an intelligent network of totally managed, business aligned, metadata driven, collaborative and inter connected data products.`,
+    heading: `Lightweight, Agile, and Collaborative`,
+    content: `Declarative nature of Intents makes is easy for providers and consumers to collaborate and iteratively evolve to intents that are fit for purpose.`,
   },
   {
-    heading: `Intelligent Network of
-Managed Micro Data Products`,
-    content: `Convert your enterprise data into an intelligent network of totally managed, business aligned, metadata driven, collaborative and inter connected data products.`,
+    heading: `Get started in minutes. No ETL or ELT mandatory`,
+    content: `Contrarty to other platforms, Dome does not require you to move your data as a pre requisite barrier in order to be able to manage it. Apply the best management frameworks, principles, and controls on data wherever it is.`,
   },
 ];
 
@@ -63,7 +56,7 @@ export default function Section1() {
             <div
               key={`${index}`}
               className={`rounded-[10px]  landingBox ${
-                index > 2 ? "sm:h-[525px] md:h-[550px] lg:h-[620px]" : "sm:h-[600px]  md:h-[550px] lg:h-[660px]"
+                index > 2 ? "sm:h-[555px] md:h-[560px] lg:h-[640px]" : "sm:h-[600px]  md:h-[560px] lg:h-[660px]"
               }`}
             >
               <div className="w-full">
@@ -72,7 +65,7 @@ export default function Section1() {
               </div>
               </div>
               <div className="h-[50%] mt-8">
-                <div className="px-10 text:text-[16px] lg:text-[20px] font-[700] h-[30%]">
+                <div className={`px-10 text:text-[16px] lg:text-[20px] font-[700] ${index >2 ? "h-[25%]":"h-[30%]"} `}>
                   {element.heading}
                 </div>
                 <div className="px-10 text-[16px] lg:text-[18px] text-[#555] h-[60%]">
