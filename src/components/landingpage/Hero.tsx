de5b93445d935079bf3d656ac7cb8c6e5d40ec4f
Umang Rajawat <umang.rajawat@gupshup.io>
2023-09-01 16:17:54 +0530

    video m4v

--- src/components/landingpage/Hero.tsx
+++ src/components/landingpage/Hero.tsx
@@ -36,31 +36,7 @@ export default function Hero() {
 
   return (
     <div>
-      {/* <div className="sm:hidden lg:flex relative items-center justify-center h-[800px] overflow-hidden">
-        <div className="relative  z-30 sm:mt-[-42rem] lg:mt-[-18rem] -mt-[8rem] 2xl:mt-[-19rem] rounded-xl flex flex-col justify-center items-center">
-          <p className="sm:text-[30px] sm:text-center lg:text-[40px] 2xl:text-[60px] text-[72px] font-[700] sm:leading-9">
-            Manage your Data assets with ease at scale
-          </p>
-          <p className="sm:text-[10px] lg:text-[24px] sm:mt-4 lg:mt-10 flex">
-            Convert your data into{" "}
-            <div className="text-[#4D91FF] change-text flex justify-center font-[700] lg:w-[150px] sm:w-[70px]">
-              {array[wordRef.current]}{" "}
-            </div>
-            Business aligned Data Products in minutes.
-          </p>
-        </div>
-        <video
-          autoPlay
-          loop
-          muted
-          className="absolute sm:top-[-18rem] lg:top-[-6rem] xl:top-[-6rem] 2xl:top-[-5.5rem] 3xl:top-0 z-10 w-[150vw] h-[700px]"
-        >
-          <source src="/landingPage/Homevideo-Comp.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div> */}
-
-      <div className="relative flex items-center justify-center h-[800px] overflow-hidden">
+       <div className="relative flex items-center justify-center h-[800px] overflow-hidden">
         <div className="relative  z-30  sm:mt-[-35rem] lg:mt-[-18rem] -mt-[8rem] 2xl:mt-[-19rem] rounded-xl flex flex-col justify-center items-center">
           <p className="sm:text-[30px] sm:px-4 sm:text-center lg:text-[40px] 2xl:text-[60px] text-[72px] font-[700] sm:leading-9">
             Manage your Data assets with ease at scale
@@ -93,6 +69,7 @@ export default function Hero() {
           className="absolute sm:top-[-18rem] lg:top-[-6rem] xl:top-[-6rem] 2xl:top-[-5.5rem] 3xl:top-0 z-10 w-[150vw] h-[700px]"
         >
           <source src="/landingPage/Homevideo-Comp.mp4" type="video/mp4" />
+          <source src="/landingPage/Homevideo-Comp.m4v" type="video/x-m4v" />
           Your browser does not support the video tag.
         </video>
       </div>
